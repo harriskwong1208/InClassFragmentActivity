7af57fa5ca19229e298789dfbada0eba48900da4
Harris Kwong <harriskwong120898@gmail.com>
2023-03-23 12:16:36 -0400

    Completed ViewModel Activity.

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -3,21 +3,29 @@ package edu.temple.inclassactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentContainerView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
 
+        mainViewModel.setImageIds(imageArray)
+
         // Attach an instance of ImageDisplayFragment using factory method
-        val fragment = ImageDisplayFragment.newInstance(imageArray)
+
+
         val fragmentContainView = findViewById<FragmentContainerView>(R.id.fragmentContainerView3)
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView3,fragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView3,ImageDisplayFragment())
+            .commit()
 
     }
 }
